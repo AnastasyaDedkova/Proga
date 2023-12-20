70a5e67f8e558a831f48e47ed890a8ba2bc01812
AnastasyaDedkova <dedkova_smolensk@mail.ru>
2023-12-21 01:41:51 +0300

    feat: test task1

---
+++ tests/lab4/test_r_sys.py
@@ -0,0 +1,18 @@
+import unittest
+from src.lab4.r_sys import *
+
+class R_sysTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.movies_file = "movies.txt"
+        self.viewing_history_file = "viewing_history.txt"
+
+    def test_recommend_movie(self):
+        system = RecommendationSystem(self.movies_file, self.viewing_history_file)
+
+        # Assuming that Movie objects are created with IDs 1, 2, 3 for simplicity
+        user_viewed_movies = [1, 2]
+        recommendation = system.recommend_movie(user_viewed_movies)
+
+        self.assertEqual(recommendation, "Дюна")
+
